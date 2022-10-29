07a535ee896855520983d45093fbab596d5319a7
Yen Xiong Yuan <yenyuan17@gmail.com>
2022-10-29 03:16:14 +0000

    pass sumArray test

--- js/app.js
+++ js/app.js
@@ -61,7 +61,10 @@ testSumAndMultiply(4,7,5);
 
 /////////////////////////////////////
 /* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the 
+
+first element is the sum of the numbers in the array, and 
+the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
@@ -73,12 +76,18 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+  let numOne= testArray[0];
+  let numTwo= testArray[1];
+  let numThree= testArray[2];
+  // console.log(sum(numOne + numTwo, numThree), `${testArray} was passed in as an array numbers, and ${numOne + numTwo + numThree} is their sum.`);
+
+  return [sum(numOne + numTwo,  numThree)[0], `${testArray} was passed in as an array of numbers, and ${numOne + numTwo + numThree} is their sum.`];
 
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
